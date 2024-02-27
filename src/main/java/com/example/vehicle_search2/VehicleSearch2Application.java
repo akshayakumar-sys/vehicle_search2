2c2aa952e465b06dd93ad5a82b74f270bac54fe4
akshayakumar-sys <akshaykumaryadahalli@gmai.com>
2024-02-28 00:41:12 +0530

    project initial setup

---
+++ src/main/java/com/example/vehicle_search2/VehicleSearch2Application.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_search2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearch2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehicle_search2/VehicleSearch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_search2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
